e9cfbfcb5761bb01e2fec8fc2dde0eb3a72210ba
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-05-03 02:09:01 +0200

    Trim

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+    "PyQt5==5.8.2",
+    "pdf-table-extractor==0.1.1"
 ]
 
 test_requirements = [
@@ -40,13 +41,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     tests_require=test_requirements
